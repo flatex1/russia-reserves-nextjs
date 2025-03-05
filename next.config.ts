9b1bc4167d131b3a1a5749cfa2c5854f0f29442b
flatex <flatex@duck.com>
2025-03-05 23:40:32 +0300

    fix nextconfig

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ['youthful-manatee-762.convex.cloud'],
+    domains: ['youthful-manatee-762.convex.cloud', 'opulent-mammoth-378.convex.cloud'],
   },
 };
 
